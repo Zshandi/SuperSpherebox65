48fb0a7189d8248d570a17b3aa3ef7d937f27eaf
Zshandi Krahn <ZshandiK@gmail.com>
2024-01-27 14:06:25 -0800

    More updates for game selection

--- GameSelect.gd
+++ GameSelect.gd
@@ -6,8 +6,6 @@ var game_loader:GameLoader = $"../GameLoader"
 
 var current_position:Vector2i = Vector2i.ZERO
 
-var current_selection:Game
-
 func _ready():
-	var scene := preload("res://game_1.tscn").instantiate()
-	game_loader.load_game(scene)
+	
+	pass
