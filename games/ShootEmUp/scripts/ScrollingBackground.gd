6bc53449b640b3ddd34091b11067fb30272d1c98
Cameron Smith <59268172+dumbanode@users.noreply.github.com>
2024-01-27 17:55:07 -0800

    Started creating player for shoot em up

--- games/ShootEmUp/scripts/ScrollingBackground.gd
+++ games/ShootEmUp/scripts/ScrollingBackground.gd
@@ -1,9 +1,25 @@
 extends ParallaxBackground
 
+@export
+var back_images: Array[Texture2D] = []
+@export
+var front_images: Array[Texture2D] = []
+
+
 @onready var back_sprite = $BackLayer/Sprite2D
 @onready var front_sprite = $FrontLayer/Sprite2D
 @onready var scroll_speed_back = Vector2(10,10)
 @onready var scroll_speed_front = Vector2(30,30)
+
+func _ready():
+	# choose the front and back images
+	back_sprite.texture = back_images.pick_random()
+	front_sprite.texture = front_images.pick_random()
+	
+	# modulate the colors
+	back_sprite.modulate = Color(randf(), randf(), randf(), randf())
+	front_sprite.modulate = Color(randf(), randf(), randf(), randf())
+
 
 func _process(delta):
 	back_sprite.region_rect.position += delta * scroll_speed_back
