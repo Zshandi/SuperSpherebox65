8d0f171065838d6f19f7df5009be9fd1496dffcb
Cameron Smith <smac1994@gmail.com>
2024-02-21 06:58:14 -0800

    Update EnemyLaser.gd

--- games/ShootEmUp/scripts/EnemyLaser.gd
+++ games/ShootEmUp/scripts/EnemyLaser.gd
@@ -1,11 +1,11 @@
 extends Laser
 	
-func _move_forward(delta):
+func _move_forward(delta: int) -> void:
 	global_position.y += fire_speed * delta
 
 
 
-func _on_body_entered(body):
+func _on_body_entered(body) -> void:
 	if (body is Player):
 		body.take_damage()
 		queue_free()
