8a6c490424211d31547ba6612a8a7525931accaf
Zshandi Krahn <ZshandiK@gmail.com>
2024-02-29 22:10:08 -0800

    Fix issue with the game not properly restoring the rng state if a weird fortune has been seen
    Also add some comments regarding call sequence during game initialization

--- games/EightBall/scripts/eight_ball.gd
+++ games/EightBall/scripts/eight_ball.gd
@@ -33,6 +33,8 @@ var weird_fortune_chance: float = 1
 
 var fortunes:Array[String]
 
+var next_fortune:String = ""
+
 var weird_fortune:WeirdFortune
 var weird_fortune_num:int = Main.INT_MAX
 
@@ -42,21 +44,9 @@ var gone_phrase:String
 
 var is_gone:bool = false
 
-var num_fortunes:int = 1
+var num_fortunes:int = 0
 
-func _load_save_data(save_data:Dictionary):
-	if save_data.has("num_fortunes"):
-		num_fortunes = save_data["num_fortunes"]
-
-func save_game_data():
-	var save_dict := {}
-	save_dict["num_fortunes"] = num_fortunes
-	save_game.emit(save_dict)
-
-func _ready():
-	background.sprite.modulate = Color(randf(),randf(),randf(),1)
-	background.color_rect.color = Color(randf(),randf(),randf(),1)
-	
+func _on_game_initialized():
 	# grab fortunes
 	good_fortune_to_use = possible_good_fortunes.pick_random()
 	bad_fortune_to_use = possible_bad_fortunes.pick_random()
@@ -87,54 +77,87 @@ func _ready():
 		weird_fortune_num = randi_range(weird_fortune.num_fortune_min_max[0], weird_fortune.num_fortune_min_max[1])
 		print_debug("\nweird fortune: ", weird_fortune.fortune_sequence)
 		print_debug("weird fortune num: ", weird_fortune_num)
+
+func _load_save_data(save_data:Dictionary):
+	if save_data.has("num_fortunes"):
+		var loaded_num_fortunes = save_data["num_fortunes"]
+		print_debug("\n\nloaded_num_fortunes: ", loaded_num_fortunes)
+		
+		# Get the rng back to state the game was last played,
+		#  and also the num_fortunes back to what it previously was
+		for i in range(loaded_num_fortunes):
+			randomize_fortune()
+		
+	print_debug("\n\nnum_fortunes: ", num_fortunes)
+
+func save_game_data():
+	var save_dict := {}
+	save_dict["num_fortunes"] = num_fortunes
+	save_game.emit(save_dict)
+
+func _ready():
+	background.sprite.modulate = Color(randf(),randf(),randf(),1)
+	background.color_rect.color = Color(randf(),randf(),randf(),1)
 	
 	# grab image to use
 	eight_ball_sprite.texture = game_instance_data.game_image_2
 	eight_ball_sprite.modulate = game_instance_data.game_image_color_2
-	
-	# Get the rng back to state the game was last played
-	for i in range(num_fortunes):
-		_randomize_fortune()
 	
 	if is_gone:
 		# Don't play leaving animation,
 		#  if already gone when started
 		eight_ball_sprite.hide()
 	
-	_randomize_fortune()
+	# Determine fortune before it's asked for
+	randomize_fortune()
+	game_over.set_fortune(next_fortune)
 
 
 func _process(delta):
 	if Input.is_action_just_pressed("six"):
 		if not game_over.is_visible():
+			
+			# Show the fortune screen
 			game_over.show()
-			print_debug("num_fortunes: ", num_fortunes)
-			num_fortunes += 1
+			
+			# Although num_fortunes is incremented when the
+			#  screen is closed, we only save it once it's re-opened
 			save_game_data()
+			print_debug("num_fortunes: ", num_fortunes)
+			
 		else:
+			# Play the leaving animation if is_gone,
+			#  but only if the sprite is not already hidden or playing
 			if is_gone && eight_ball_sprite.is_visible() && !$AnimationPlayer.is_playing():
 				$AnimationPlayer.play("leave")
 				$AnimationPlayer.animation_finished.connect( func(_n):
 					eight_ball_sprite.hide() )
+			
+			# Hide the fortune screen (labelled as game_over)
 			game_over.hide()
-			_randomize_fortune()
+			
+			# Determine next fortune before it's asked for
+			randomize_fortune()
+			game_over.set_fortune(next_fortune)
 
-func _randomize_fortune():
+func randomize_fortune():
+	num_fortunes += 1
+	
 	if generate_leaving_fortune(): return
 	
 	if generate_weird_fortune(): return
 	
-	game_over.set_fortune(fortunes.pick_random())
+	next_fortune = fortunes.pick_random()
 
 func generate_leaving_fortune() -> bool:
 	if num_fortunes == leave_fortune_num:
-		game_over.set_fortune(leaving_phrase)
+		next_fortune = leaving_phrase
 		is_gone = true
 		return true
 	
 	if num_fortunes > leave_fortune_num:
 		is_gone = true
-		game_over.set_fortune(gone_phrase)
+		next_fortune = gone_phrase
 		return true
 	
 	return false
@@ -145,7 +168,7 @@ func generate_weird_fortune() -> bool:
 	if index >= 0:
 		var fortune_seq = weird_fortune.fortune_sequence
 		if index < fortune_seq.size():
-			game_over.set_fortune(fortune_seq[index])
+			next_fortune = fortune_seq[index]
 			return true
 	
 	return false
