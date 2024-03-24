1595288c2dfb671624fc1f3aea4e851f796038c9
Zshandi Krahn <ZshandiK@gmail.com>
2024-03-23 21:58:26 -0700

    Update eightball to use new `Rand` class

--- games/EightBall/scripts/eight_ball.gd
+++ games/EightBall/scripts/eight_ball.gd
@@ -60,44 +60,44 @@ func save_game_data():
 	save_game.emit(save_dict)
 
 func _ready():
-	background.sprite.modulate = Color(randf(),randf(),randf(),1)
-	background.color_rect.color = Color(randf(),randf(),randf(),1)
+	background.sprite.modulate = Rand.color()
+	background.color_rect.color = Rand.color()
 	
 	# grab fortunes
-	good_fortune_to_use = possible_good_fortunes.pick_random()
-	bad_fortune_to_use = possible_bad_fortunes.pick_random()
-	uncertain_fortune_to_use = possible_uncertain_fortunes.pick_random()
+	good_fortune_to_use = Rand.pick(possible_good_fortunes)
+	bad_fortune_to_use = Rand.pick(possible_bad_fortunes)
+	uncertain_fortune_to_use = Rand.pick(possible_uncertain_fortunes)
 	
 	fortunes = [good_fortune_to_use, bad_fortune_to_use, uncertain_fortune_to_use]
 	
 	# determine if and when we will leave, and what to say
-	if randf() < chance_to_leave:
-		leave_fortune_num = randi_range(min_max_leave_time[0], min_max_leave_time[1])
-		leaving_phrase = possible_leaving_phrases.pick_random()
-		gone_phrase = possible_gone_phrases.pick_random()
+	if Rand.f() < chance_to_leave:
+		leave_fortune_num = Rand.i_range(min_max_leave_time[0], min_max_leave_time[1])
+		leaving_phrase = Rand.pick(possible_leaving_phrases)
+		gone_phrase = Rand.pick(possible_gone_phrases)
 		print_debug("\nleaving fortune: ", [leaving_phrase, gone_phrase])
 		print_debug("leaving fortune num: ", leave_fortune_num)
 	
 	# determine if we should have a weird fortune, and which one we should use
-	if randf() < weird_fortune_chance:
+	if Rand.f() < weird_fortune_chance:
 		var total_weight:float = 0
 		for fortune:WeirdFortune in weird_fortunes:
 			total_weight += fortune.fortune_chance_weight
-		var chance = randf_range(0, total_weight)
+		var chance = Rand.f_range(0, total_weight)
 		total_weight = 0
 		for fortune:WeirdFortune in weird_fortunes:
 			if chance - total_weight <= fortune.fortune_chance_weight:
 				weird_fortune = fortune
 				break
 			total_weight += fortune.fortune_chance_weight
-		weird_fortune_num = randi_range(weird_fortune.num_fortune_min_max[0], weird_fortune.num_fortune_min_max[1])
+		weird_fortune_num = Rand.i_range(weird_fortune.num_fortune_min_max[0], weird_fortune.num_fortune_min_max[1])
 		print_debug("\nweird fortune: ", weird_fortune.fortune_sequence)
 		print_debug("weird fortune num: ", weird_fortune_num)
 		
 		# ensure the weird fortune comes before the leaving fortune
 		var weird_fotune_end = weird_fortune_num + weird_fortune.fortune_sequence.size()
 		if leave_fortune_num < weird_fotune_end:
-			leave_fortune_num = weird_fotune_end + randi_range(0, 7)
+			leave_fortune_num = weird_fotune_end + Rand.i_range(0, 7)
 			print_debug("bumped leave fortune num to: ", leave_fortune_num)
 		
 	
@@ -156,7 +156,7 @@ func randomize_fortune():
 	
 	if generate_weird_fortune(): return
 	
-	next_fortune = fortunes.pick_random()
+	next_fortune = Rand.pick(fortunes)
 
 func generate_leaving_fortune() -> bool:
 	if num_fortunes == leave_fortune_num:
--- games/EightBall/scripts/eight_ball_game_data.gd
+++ games/EightBall/scripts/eight_ball_game_data.gd
@@ -22,23 +22,23 @@ var multiple_chance:float
 var multiple_max:int = 4
 
 func _post_draw_card_image(parent_control:Control, instance_date:GameInstanceData) -> void:
-	instance_date.game_image_2 = eight_ball_images.pick_random()
+	instance_date.game_image_2 = Rand.pick(eight_ball_images)
 	
 	var num_sprites:int = 1
-	if randf() < multiple_chance:
-		num_sprites = randi_range(2, multiple_max)
+	if Rand.f() < multiple_chance:
+		num_sprites = Rand.i_range(2, multiple_max)
 	
 	var scale := Vector2.ONE / sqrt(num_sprites)
-	if randf() < scale_chance:
-		scale *= randf_range(min_scale, max_scale)
+	if Rand.f() < scale_chance:
+		scale *= Rand.f_range(min_scale, max_scale)
 	
-	var rotate := randf() < rotation_chance
+	var rotate := Rand.f() < rotation_chance
 	
 	for i in range(0, num_sprites):
 		var sprite := draw_texture_random(instance_date.game_image_2, parent_control)
 		sprite.modulate = instance_date.game_image_color_2
 		
 		if rotate:
-			sprite.rotation = randf_range(0, TAU)
+			sprite.rotation = Rand.f_range(0, TAU)
 		
 		sprite.scale = scale
