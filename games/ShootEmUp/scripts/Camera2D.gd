0ac2c8e6fb5f745c03ad2bb9a5a0783736d05a88
Cameron Smith <59268172+dumbanode@users.noreply.github.com>
2024-03-25 06:22:13 -0700

    Apply suggestions from code review
    Co-authored-by: Zshandi Krahn <ZshandiK@gmail.com>

--- games/ShootEmUp/scripts/Camera2D.gd
+++ games/ShootEmUp/scripts/Camera2D.gd
@@ -2,19 +2,22 @@ extends Camera2D
 
 var shake_strength: float = 0.0
 
-const SHAKE_DURATION: float = 5.0
-const DEFAULT_SCREEN_SHAKE: float = 30.0
+var shake_decay = DEFAULT_SHAKE_DECAY
 
-func shake_screen(strength: int = 0) -> void:
-	if strength == 0:
-		shake_strength = DEFAULT_SCREEN_SHAKE
-	else:
-		shake_strength = strength
+const DEFAULT_SHAKE_DECAY: float = 5.0
+const DEFAULT_SHAKE_STRENGTH: float = 30.0
+
+func shake_screen(strength: float = DEFAULT_SHAKE_STRENGTH, decay: float = DEFAULT_SHAKE_DECAY) -> void:
+	shake_strengh = strength
+	shake_decay = decay
 	
 func _process(delta):	
 	if shake_strength > 0:
-		shake_strength = lerpf(shake_strength, 0 , SHAKE_DURATION * delta)
+		shake_strength = lerpf(shake_strength, 0 , shake_decay * delta)
+		shake_strength = max(shake_strength, 0)
 		offset = Vector2(
 			randf_range(-shake_strength, shake_strength),
 			randf_range(-shake_strength, shake_strength)
 		)
+	else:
+		offset = Vector2.ZERO
